c16b35032674fad146c021808aca25675ca2e2ee
Edward Liao <edward.liao@blackbook.ai>
2021-04-28 19:53:28 +1000

    Change conversion not to include whitespace, tabs to spaces

--- index.js
+++ index.js
@@ -8,34 +8,36 @@ const min = 20
 const max = limit 
 
 function minToTimeString(mins) {
-	const hr = Math.floor(mins / 60)
-	const m = mins % 60
-	const str = (!!hr ? `${hr} hr ` : '') + (!!m ? `${m} min`: '')
-	return str
+  const hr = Math.floor(mins / 60)
+  const m = mins % 60
+  const stringArray = []
+  if(!!hr) stringArray.push(`${hr} hr`)
+  if(!!m) stringArray.push(`${m} min`)
+  return stringArray.join(' ')
 }
 
 function makeModules() {
-	const modules = [] 
-	for(let i = 0; i < lines; i++) {
-		const time = Math.floor(Math.random() * (max - min) + min)
-		modules.push(minToTimeString(time))
-	}
-	return modules
+  const modules = []
+  for(let i = 0; i < lines; i++) {
+    const time = Math.floor(Math.random() * (max - min) + min)
+    modules.push(minToTimeString(time))
+  }
+  return modules
 }
 
 function writeFile(modules) {
-	return new Promise((resolve, reject) => {
-		const stringifiedModules = modules.join('\n')
-		const dataString = limit + '\n' + lines + '\n'
-		const data = dataString + stringifiedModules
-		fs.appendFile('data.txt', data, (err) => {
-			if(err) {
-				reject(err)
-			} else {
-				resolve(data.length)
-			}
-		})
-	})
+  return new Promise((resolve, reject) => {
+    const stringifiedModules = modules.join('\n')
+    const dataString = limit + '\n' + lines + '\n'
+    const data = dataString + stringifiedModules
+    fs.writeFile('data.txt', data, (err) => {
+      if(err) {
+        reject(err)
+      } else {
+        resolve(data.length)
+      }
+    })
+  })
 }
 
 const readDataAndCallSolution = () => {
--- solution.js
+++ solution.js
@@ -1,41 +1,41 @@
 const expr = /(\d+ hr)?(?:\s)?(\d+ min)?/;
 function timeStringToMins(str) {
-	const result = expr.exec(str)
-	if(result) {
-		const [_, hrStr, minStr] = result
-		const hours = hrStr ? parseInt(hrStr.split(' ')[0], 10) * 60 : 0
-		const mins = minStr ? parseInt(minStr.split(' ')[0], 10) : 0
-		return hours + mins
-	} else {
-		return 0
-	}
+  const result = expr.exec(str)
+  if(result) {
+    const [_, hrStr, minStr] = result
+    const hours = hrStr ? parseInt(hrStr.split(' ')[0], 10) * 60 : 0
+    const mins = minStr ? parseInt(minStr.split(' ')[0], 10) : 0
+    return hours + mins
+  } else {
+    return 0
+  }
 }
 
 function solution(data, limit, lines) {
-	const days = []
+  const days = []
 
-	let buffer = []
-	let daySum = 0
+  let buffer = []
+  let daySum = 0
 
-	let cursor = 0;
-	while(cursor < lines) {
-		const moduleTime = timeStringToMins(data[cursor])
-		console.log(moduleTime)
+  let cursor = 0;
+  while(cursor < lines) {
+    const moduleTime = timeStringToMins(data[cursor])
+    console.log(moduleTime)
 
-		if((daySum + moduleTime) > limit) {
-			days.push(buffer)
-			buffer = []
-			daySum = 0
-		}
+    if((daySum + moduleTime) > limit) {
+      days.push(buffer)
+      buffer = []
+      daySum = 0
+    }
 
-		buffer.push(moduleTime)
-		daySum += moduleTime
+    buffer.push(moduleTime)
+    daySum += moduleTime
 
-		cursor++
-	}
-	days.push(buffer)
-	console.log(days)
-	return days.length
+    cursor++
+  }
+  days.push(buffer)
+  console.log(days)
+  return days.length
 }
 module.exports = {
   solution
